a9d6fa28207039daf9bd0311daead26f4ba2c3f8
PunreachFNSV <punreachrany@fnsvalue.co.kr>
2021-04-14 11:07:10 +0900

    Addition

---
+++ Manager.swift
@@ -0,0 +1,21 @@
+//
+//  Manager.swift
+//  PunreachSpeaking
+//
+//  Created by elite on 2021/04/14.
+//
+
+import Foundation
+import UIKit
+
+public class Manager {
+    public init(){}
+    
+    public func punreachQouting() {
+        print("Never miss an opportunity to awesome")
+    }
+    
+    public func getPunreachQouting() -> String {
+        return "Never miss an opportunity to awesome"
+    }
+}
